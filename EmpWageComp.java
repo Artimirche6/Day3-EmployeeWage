c08bc07e9a72d29515eaefbe1ffd2ace6b40f552
Artimirche6 <artimirche6@gmail.com>
2021-09-30 14:25:24 +0530

    [arti] Employee Part

--- EmpWageComp.java
+++ EmpWageComp.java
@@ -2,17 +2,37 @@ public class EmpWageComp
 {
 	public static void main(String[] args)
 	{
-		int IS_PRESENT=1;
 
-		double empCheck=(Math.floor(Math.random() * 10) % 2);
+		int IS_FULL_TIME=1;
+		int IS_PART_TIME=2;
 
-		if ( empCheck == IS_PRESENT )
-		{
-			System.out.println("Employee is Present");
-		}	
-		else
-		{
-			System.out.println("Employee is Absent");
-		}
+		int EMP_RATE_PER_HOUR=20;
+		int empHrs=0;
+		int empWage=0;
+
+		int empCheck = (int) (Math.floor(Math.random()*10)%3);
+
+ 		System.out.println("EmpCheck : "+empCheck);
+
+		if(empCheck == IS_FULL_TIME)
+ 		{
+	   	empHrs=16;
+	   	System.out.println("Employee is Present");
+	   }
+		else if(empCheck == IS_PART_TIME)
+ 		{
+	  		empHrs=8;
+			System.out.println("Employee is Part Time Present");
+ 		}
+ 		else
+ 		{    
+	 		empHrs=0;
+	   	System.out.println("Employee is Absent");
+ 		}
+
+ 	empWage=empHrs*EMP_RATE_PER_HOUR;
+ 	System.out.println("Employee Wage : "+empWage);
+	
 	}
+
 }
