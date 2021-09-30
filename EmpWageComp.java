8176ac2f4d6dd9c9022a5d047a0c960699f1d0b4
Artimirche6 <artimirche6@gmail.com>
2021-09-30 14:31:19 +0530

    [art] Welcome to Employee Wage

--- EmpWageComp.java
+++ EmpWageComp.java
@@ -2,17 +2,6 @@ public class EmpWageComp
 {
 	public static void main(String[] args)
 	{
-		int IS_PRESENT=1;
-
-		double empCheck=(Math.floor(Math.random() * 10) % 2);
-
-		if ( empCheck == IS_PRESENT )
-		{
-			System.out.println("Employee is Present");
-		}	
-		else
-		{
-			System.out.println("Employee is Absent");
-		}
+			System.out.println("Welcome to Employee Wage Computation");
 	}
 }
