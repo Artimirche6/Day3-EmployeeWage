816b8195d2ce40508c7ac5c0c69e965e11360260
Artimirche6 <artimirche6@gmail.com>
2021-10-01 18:21:06 +0530

    [arti] Employee Wage Master

--- EmpWageComp.java
+++ EmpWageComp.java
@@ -2,7 +2,7 @@ public class EmpWageComp
 {
 	public static void main(String args[])
 	{
-			System.out.println("Welcome to Employee Wage Computation in Master");
+			System.out.println("Welcome to Employee Wage Computation");
 	}
 
 }
